ec00106599cca09f1564d67dad135f152add267a
David <BlueCodium@proton.me>
2022-10-05 21:18:06 +0200

    Relocalisation de la classe `Banners(` en un fichier distinct le séparant du fichier `home_body.dart`

---
+++ lib/home/banners.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Banners extends StatelessWidget {
+  //Banniere du haut des Cerveaux
+  const Banners({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Image.asset(
+              'assets/banniere_bible.jpg',
+              width: 375,
+            ),
+            const Padding(padding: EdgeInsets.all(10)),
+            Image.asset(
+              'assets/banniere_milliard.jpg',
+              width: 375,
+            ),
+            const Padding(padding: EdgeInsets.all(10)),
+            Image.asset(
+              'assets/banniere_miracle.jpg',
+              width: 375,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
--- lib/home/home_body.dart
+++ lib/home/home_body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../var.dart';
+import 'banners.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
@@ -12,38 +12,6 @@ class HomeBody extends StatelessWidget {
           children: const [
             Banners(),
             Padding(padding: EdgeInsets.all(10)),
-          ],
-        )
-      ],
-    );
-  }
-}
-
-class Banners extends StatelessWidget {
-  //Banniere du haut des Cerveaux
-  const Banners({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Column(
-          children: [
-            Image.asset(
-              'assets/banniere_bible.jpg',
-              width: 375,
-            ),
-            const Padding(padding: EdgeInsets.all(10)),
-            Image.asset(
-              'assets/banniere_milliard.jpg',
-              width: 375,
-            ),
-            const Padding(padding: EdgeInsets.all(10)),
-            Image.asset(
-              'assets/banniere_miracle.jpg',
-              width: 375,
-            )
           ],
         )
       ],
